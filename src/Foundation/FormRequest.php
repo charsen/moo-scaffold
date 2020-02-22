45960de4cf6d5b5baba775f937cd705cbc19d4f7
Charsen <780537@gmail.com>
2020-02-22 17:18:35 +0800

    将 getFormWidgtets 移到 FormRequest 中

--- src/Foundation/FormRequest.php
+++ src/Foundation/FormRequest.php
@@ -45,12 +45,84 @@ class FormRequest extends BaseFormRequest
     }
 
     /**
+     * 获取通过某动作的验证规则转换的表单控件
+     *
+     * @param  array  $reset
+     * @return array
+     */
+    public function getFormWidgets($method, array $reset = [])
+    {
+        $all_rules      = $this->getActionRules($method);
+        $frontend_rules = []; //$this->getFrontendRules($all_rules);
+
+        return $this->transformFormWidgets($all_rules, $frontend_rules, $reset);
+    }
+
+    /**
+     * 根据验证规则转换成表单控件
+     *
+     * @param  array $all_rules
+     * @param  array $frontend_rules
+     * @param  array $reset
+     * @return array
+     */
+    private function transformFormWidgets(array $all_rules, array $frontend_rules, array $reset = [])
+    {
+        if (empty($all_rules)) return [];
+
+        $result = [];
+        foreach ($all_rules as $field_name => $rules) {
+            $tmp = [
+                'id'        => $field_name,
+                'required'  => ! (in_array('sometimes', $rules) OR in_array('nullable',  $rules)),
+                'rules'     => $frontend_rules[$field_name] ?? [],
+            ];
+
+            // 通过字段类型指定 控件类型, todo: datetime?
+            if (in_array('data', $rules)) {
+                $tmp['type'] = 'date-picker';
+            }
+
+            if (strstr($field_name, 'password')) {
+                $tmp['type'] = 'password';
+            }
+
+            foreach ($rules as $r) {
+                // 闭包函数
+                if ( ! is_string($r)) {
+                    continue;
+                }
+
+                if (preg_match('/^in\:[0-9\,]+$/i', $r)) {
+                    // 判断 model 的字典字段
+                    if (property_exists($this, "init_" . $field_name)) {
+                        $tmp['type']        = 'radio';
+                        $tmp['dictionary']  = true;
+                        $tmp['options']     = $this->{"init_{$field_name}"};
+                    }
+                }
+            }
+
+            // 直接替换，合并
+            if (isset($reset[$field_name])) {
+                $tmp = array_merge($tmp, $reset[$field_name]);
+                unset($reset[$field_name]);
+            }
+
+            $result[$field_name] = $tmp;
+        }
+
+        // 若 reset 还存在数据，则是新增的，直接附加进去
+        return \array_merge($result, $reset);
+    }
+
+    /**
      * 转换成前端验证规则
      *
      * @param  array $all_rules
      * @return array
      */
-    public function getFrontendRules(array $all_rules): array
+    private function getFrontendRules(array $all_rules): array
     {
         if (empty($all_rules)) return [];
 
@@ -86,25 +158,24 @@ class FormRequest extends BaseFormRequest
     /**
      * 获取模型某字典字段的键值
      *
-     * @param  \Illuminate\Database\Eloquent\Model $model
      * @param  $filed
      * @return string
      */
-    public function getDictKeys($model, $filed): string
+    private function getDictKeys($filed): string
     {
-        return implode(',', array_keys($model->{"init_{$filed}"}));
+        //return implode(',', array_keys($this->{"init_{$filed}"}));
+        return implode(',', array_keys($this->{"init_{$filed}"}));
     }
 
     /**
      * 获取模型字典字段的 In 验证规则
      *
-     * @param \Illuminate\Database\Eloquent\Model $model
      * @param string $filed
      * @return string
      */
-    public function getInDict($model, $filed): string
+    protected function getInDict($filed): string
     {
-        return 'in:' . $this->getDictKeys($model, $filed);
+        return 'in:' . $this->getDictKeys($filed);
     }
 
     /**
@@ -115,9 +186,9 @@ class FormRequest extends BaseFormRequest
      * @param  $route_key       // 当是编辑动作时，要排除自己，必传
      * @return string
      */
-    public function getUnique($model, $field = null, $route_key = null): string
+    protected function getUnique($field = null, $route_key = null): string
     {
-        $rule = 'unique:' . $model->getTable();
+        $rule = 'unique:' . $this->model_table;
 
         if ($field != null && $route_key != null)
         {
